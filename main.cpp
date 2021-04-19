4fc0e38e88f7fd831811eda7d02b436d048b2b4c
LAB (Andriyanova KB) <krisandry2002@gmail.com>
2021-04-19 14:54:33 +0300

    input_numbers

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    size_t  number_count;
+    cerr <<  "Enter number count: ";
+    cin >>  number_count;
+    vector < double > numbers (number_count);
+    cerr << "numbers=";
+    for (size_t i=0; i<number_count; i++)
+        cin>>numbers[i];
+    size_t SCREEN_WIDTH;
+    cerr << "Enter SCREEN_WIDTH:";
+    cin >> SCREEN_WIDTH;
+    while(SCREEN_WIDTH < 7 || SCREEN_WIDTH > 80 || SCREEN_WIDTH < 1/3*(number_count))
+    {
+        cerr << "invalid input, please enter again";
+        cerr << "Enter SCREEN_WIDTH:";
+        cin >> SCREEN_WIDTH;
+    }
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+    size_t  bin_count;
+    cerr <<  "Enter bin_count: ";
+    cin >>  bin_count;
+    vector <size_t> bins (bin_count);
+    double min = numbers[0];
+    double max = numbers[0];
+    for (double x : numbers)
+    {
+        if (x < min)
+        {
+            min = x;
+        }
+        else if (x > max)
+        {
+            max = x;
+        }
+    }
+    double bin_size = (max - min) / bin_count;
+    for (size_t  i = 0; i < number_count; i++)
+    {
+        bool  found = false;
+        for (size_t j = 0; (j < bin_count - 1) && !found; j++)
+        {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if ((lo <= numbers[i]) && (numbers[i] < hi))
+            {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            bins[bin_count - 1]++;
+        }
+    }
+    size_t  Max = bins[0];
+    for (size_t  j=1; j<bin_count; j++)
+    {
+        if (bins[j]>Max)
+            Max = bins[j];
+    }
+    for (size_t  j=0; j<bin_count; j++)
+    {
+        if(bins[j]>=100)
+            cout<<bins[j];
+        if (bins[j]<10)
+            cout <<"  "<< bins[j];
+        else if (bins[j]<100)
+            cout<< " "<< bins[j] ;
+        cout << "|";
+        if(number_count>MAX_ASTERISK)
+        {
+            size_t  height = MAX_ASTERISK * (static_cast<double>(bins[j]) / Max);
+            for (size_t  a=0; a<height; a++)
+            {
+                cout << "*";
+            }
+        }
+        else
+            for(size_t a=0; a<bins[j]; a++)
+                cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
