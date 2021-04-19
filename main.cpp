fc3ed0e64d11640c0c3ffd409730183834487191
LAB (Andriyanova KB <krisandry2002@gmail.com>
2021-04-19 15:50:55 +0300

    make_histogram

--- main.cpp
+++ main.cpp
@@ -11,44 +11,19 @@ vector<double> input_numbers(size_t count)
     }
     return result;
 }
-
-int main()
-{
-    size_t  number_count;
-    cerr <<  "Enter number count: ";
-    cin >>  number_count;
-    const auto numbers = input_numbers(number_count);
-    for (size_t i=0; i<number_count; i++)
-        cin>>numbers[i];
-    size_t SCREEN_WIDTH;
-    cerr << "Enter SCREEN_WIDTH:";
-    cin >> SCREEN_WIDTH;
-    while(SCREEN_WIDTH < 7 || SCREEN_WIDTH > 80 || SCREEN_WIDTH < 1/3*(number_count))
-    {
-        cerr << "invalid input, please enter again";
-        cerr << "Enter SCREEN_WIDTH:";
-        cin >> SCREEN_WIDTH;
-    }
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-    size_t  bin_count;
-    cerr <<  "Enter bin_count: ";
-    cin >>  bin_count;
-    vector <size_t> bins (bin_count);
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double x : numbers)
-    {
-        if (x < min)
-        {
-            min = x;
+void find_minmax(vector<double> numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
         }
-        else if (x > max)
-        {
-            max = x;
+        if (number > max) {
+            max = number;
         }
-    }
-    double bin_size = (max - min) / bin_count;
-    for (size_t  i = 0; i < number_count; i++)
+    }}
+void make_histogram()
+ for (size_t  i = 0; i < number_count; i++)
     {
         bool  found = false;
         for (size_t j = 0; (j < bin_count - 1) && !found; j++)
@@ -66,6 +41,32 @@ int main()
             bins[bin_count - 1]++;
         }
     }
+
+int main()
+{
+    size_t  number_count;
+    cerr <<  "Enter number count: ";
+    cin >>  number_count;
+    const auto numbers = input_numbers(number_count);
+    double min;
+    double max;
+    find_minmax(numbers, min, max);
+    size_t SCREEN_WIDTH;
+    cerr << "Enter SCREEN_WIDTH:";
+    cin >> SCREEN_WIDTH;
+    while(SCREEN_WIDTH < 7 || SCREEN_WIDTH > 80 || SCREEN_WIDTH < 1/3*(number_count))
+    {
+        cerr << "invalid input, please enter again";
+        cerr << "Enter SCREEN_WIDTH:";
+        cin >> SCREEN_WIDTH;
+    }
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+    size_t  bin_count;
+    cerr <<  "Enter bin_count: ";
+    cin >>  bin_count;
+    vector <size_t> bins (bin_count);
+    double bin_size = (max - min) / bin_count;
+
     size_t  Max = bins[0];
     for (size_t  j=1; j<bin_count; j++)
     {
