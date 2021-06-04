f0742f953d9cd85c99ef6be4a6f02436e21fd0ff
Alice (AndrianovaKB) <krisandry2002.com>
2021-06-05 00:16:04 +0300

    bool promt

--- main.cpp
+++ main.cpp
@@ -21,16 +21,16 @@ input_numbers(istream& in, size_t count)
     return result;
 }
 Input
-read_input(istream& in) {
+read_input(istream& in, bool promt) {
     Input data;
 
     cerr << "Enter number count: ";
     size_t number_count;
     in >> number_count;
-
+    if(promt)
     cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
-
+    if(promt)
     cerr << "Enter bin count: ";
     in >> data.bin_count;
     return data;
@@ -88,7 +88,7 @@ int main()
 {
     Input input;
     size_t  number_count;
-    input = read_input(cin);
+    input = read_input(cin, true);
     size_t  bin_count;
     const auto bins = make_histogram(input);
     //show_histogram_text(bins, MAX_ASTERISK);
