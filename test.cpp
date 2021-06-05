7121de08d52818b89502dbe1e7a2ee5a65de07e7
Alice (AndrianovaKB) <krisandry2002.com>
2021-06-05 21:09:39 +0300

    lab03

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
-#include <cassert>
-
 #include "histogram.h"
-
+#include "svg.h"
+#include <cassert>
+#include <math.h>
 void
-test_positive() {
+test_positive()
+{
     double min = 0;
     double max = 0;
     find_minmax({1, 2, 3}, min, max);
@@ -11,7 +12,8 @@ test_positive() {
     assert(max == 3);
 }
 void
-test_negative() {
+test_negative()
+{
     double min = 0;
     double max = 0;
     find_minmax({-1,-2, -3}, min, max);
@@ -19,7 +21,8 @@ test_negative() {
     assert(max == -1);
 }
 void
-test_similar() {
+test_similar()
+{
     double min = 0;
     double max = 0;
     find_minmax({3, 3, 3}, min, max);
@@ -27,7 +30,8 @@ test_similar() {
     assert(max == 3);
 }
 void
-test_single() {
+test_single()
+{
     double min = 0;
     double max = 0;
     find_minmax({1}, min, max);
@@ -35,16 +39,39 @@ test_single() {
     assert(max == 1);
 }
 void
-test_empty() {
+test_empty()
+{
     double min = 0;
     double max = 0;
     find_minmax({}, min, max);
 }
-
+void
+test ()
+{
+    double IMAGE_WIDTH=400;
+    image_width (5, 10, IMAGE_WIDTH, cin);
+}
+void
+test1 ()
+{
+    double IMAGE_WIDTH = 1000;
+    image_width (5, 10, IMAGE_WIDTH, cin);
+}
+void
+test2 ()
+{
+    double IMAGE_WIDTH = 30;
+    image_width (5, 10, IMAGE_WIDTH, cin);
+}
 int
-main() {
+main()
+{
+    double IMAGE_WIDTH;
     test_positive();
     test_negative();
     test_single();
-    //test_empty();
+    test_empty();
+    test();
+    test1();
+    test2();
 }
