092f048387c57bcda399cdda692d526b6fe98d8f
Alice (AndrianovaKB) <krisandry2002.com>
2021-06-08 08:35:50 +0300

    изменение функций image

--- svg.h
+++ svg.h
@@ -11,7 +11,7 @@ svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,string stroke, string fill);
 void
 svg_end();
-double input_image_width(size_t number_count, double BLOCK_WIDTH, istream& in);
-double image_width (size_t number_count, double BLOCK_WIDTH, double& IMAGE_WIDTH, istream& in);
+//double input_image_width(size_t number_count, double BLOCK_WIDTH, istream& in);
+double image_width (size_t number_count, double BLOCK_WIDTH, istream& in);
 void show_histogram_svg(const vector <size_t>& bins, double bin_count, size_t  number_count, string& stroke, string& fill);
 #endif // SVG_H_INCLUDED
